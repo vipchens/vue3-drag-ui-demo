26a645f182fc0ddae0c046fc2b6c07f034fbfcb5
ruofei <vipchens@yahoo.com>
2021-12-03 22:08:44 +0800

    替换UI框架为ant-vue

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import { createApp } from "vue";
 import mitt from "mitt";
 
 import draggable from "vuedraggable";
-import ElementPlus from 'element-plus'
-import ElementPlusIcons from '@element-plus/icons'
-import 'element-plus/dist/index.css'
+import Antd from 'ant-design-vue';
 
+import JsonForm from '@lljj/vue3-form-ant';
+
+import 'ant-design-vue/dist/antd.css';
 import './assets/css/font.css'
 
 import widgetComponents from "./widgets";
@@ -14,14 +15,14 @@ import App from "./App.vue";
 
 const app = createApp(App);
 
+app.component('json-form', JsonForm)
+
 const widgets = Object.keys(widgetComponents)
 widgets.forEach(widget => {
   app.component(widget, widgetComponents[widget])
 });
 app.component('draggable', draggable)
-app.use(ElementPlus, {
-  size: 'mini'
-})
+app.use(Antd);
 
 
 app.config.globalProperties.$EventBus = mitt();
